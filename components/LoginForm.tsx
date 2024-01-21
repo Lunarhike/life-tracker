8af1e786a4519018931035028543f83c9962d5f8
lunarhike <lunarhike@gmail.com>
2024-01-21 17:31:31 +0100

    google oauth test

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -127,10 +127,12 @@ export default function LoginForm({
         >
           Sign Up
         </button>
-        <button
-          formAction={google}
-          className="bg-green-700 rounded-md px-4 py-2 text-foreground mb-2"
-        >
+      </form>
+      <form
+        className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground"
+        action={google}
+      >
+        <button className="bg-green-700 rounded-md px-4 py-2 text-foreground mb-2">
           Sign In with Google
         </button>
         {searchParams?.message && (
